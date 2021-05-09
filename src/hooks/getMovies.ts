e810249e8f80c66e3d287ace2e59c6a8d90fb7f1
Mason Waterworth <hello@masonwaterworth.com>
2021-05-09 15:09:19 -0400

    👽️ Fix API calls

--- src/hooks/getMovies.ts
+++ src/hooks/getMovies.ts
@@ -1,10 +1,10 @@
 import {useQuery} from 'react-query';
 import axios from 'axios';
 
-const API_KEY = 'http://www.omdbapi.com/?i=tt3896198&apikey=509bc2e';
+const API_KEY = 'http://www.omdbapi.com/?apikey=509bc2e';
 
 const getMovies = async (searchTerm: string) => {
-  const {data} = await axios.get(`${API_KEY}&s=` + searchTerm);
+  const {data} = await axios.get(`${API_KEY}&s=` + searchTerm + '&type=movie');
   return data;
 };
 
