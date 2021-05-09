7806da7e307ca6da6b16b6d7d122672943fa5a69
Mason Waterworth <hello@masonwaterworth.com>
2021-05-09 19:35:37 -0400

    🐛 I am just stupid

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -29,7 +29,7 @@ export const MovieList: React.FC<MovieListProps> = ({
     if (data !== undefined && data.Response !== 'False') {
       setMovieList(data.Search);
     }
-  }, [searchTerm]);
+  }, [data]);
 
   return (
     <Flex w='100%' direction='column' alignItems='center'>
